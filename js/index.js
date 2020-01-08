51fd798128fab271c6828bc16c0030274f553473
Ella M. Holcomb <ellaholcomb@ccs.k12.in.us>
2020-01-08 16:59:54 -0500

    2020endgame

--- js/index.js
+++ js/index.js
@@ -16,3 +16,9 @@ powercellsTopScore = 0;
 ‪climb = false;
 ‪park = false;
 ‪comments = false;
+
+//Endgame (THIS YEAR)
+function climbAttempt() {
+	document.getElementById('climbS').disabled = false;
+	document.getElementById('climbF').disabled = false;
+} 
