35510624886a4efc6cafa0c323405bcd8fb180eb
Ella M. Holcomb <ellaholcomb@ccs.k12.in.us>
2020-01-08 16:13:57 -0500

    fixed endgame

--- js/index.js
+++ js/index.js
@@ -16,3 +16,9 @@ powercellsTopScore = 0;
 ‪climb = false;
 ‪park = false;
 ‪comments = false;
+
+//Endgame
+function climbAttempt() {
+	document.getElementById('climbS').disabled = false;
+	document.getElementById('climbF').disabled = false;
+}
