2b669e1710c5c0b8a249a6e4883e8349e36361c6
Sirish-Kayam <Sirish-Kayam>
2020-01-09 16:21:50 -0500

    merge

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var primaryId;
-‪var compId, teamNo, matchNo, preload, moveStart, powercellsBotScore, powercellsTopScore, powercellsInScore, powercellsBotAttempt, powercellsTopAttempt, powercellsInAttempt, revolutionsCheck, colorCheck, climb, park, comments;
+‪var compId teamNo, matchNo, preload, moveStart, powercellsBotScore, powercellsTopScore, powercellsInScore, powercellsBotAttempt, powercellsTopAttempt, powercellsInAttempt, revolutionsCheck, colorCheck, climb, park, comments;
 ‪compId = 0;
 teamNo = 0;
 matchNo = 0;
@@ -21,4 +21,9 @@ powercellsTopScore = 0;
 function climbAttempt() {
 	document.getElementById('climbS').disabled = false;
 	document.getElementById('climbF').disabled = false;
-} 
+}
+
+function powerCellCounter0() {
+	var preload = document.getElementById('powerCell0').value;
+	document.getElementById('test').innerHTML = preload;
+}
