3214bf3eaadd78e9e27e5fb7a7390ee7ed24dfde
Nayan Yarabolu <nayanyarabolu@Nayans-MacBook-Pro.local>
2020-01-09 15:36:56 -0500

    updated variables

--- js/index.js
+++ js/index.js
@@ -5,18 +5,29 @@ teamNo = 0;
 matchNo = 0;
 preload = 0;
 moveStart = false;
-powercellsBotScore = 0;
-powercellsTopScore = 0;
-‪powercellsInScore = 0;
-‪powercellsBotAttempt = 0;
-‪powercellsTopAttempt = 0;
-‪powercellsInAttempt = 0;
+
+pcAutonBotScore = 0;
+pcAutonTopScore = 0;
+‪pcAutonInScore = 0;
+‪pcAutonBotAttempt = 0;
+‪pcAutonTopAttempt = 0;
+‪pcAutonInAttempt = 0;
+
+pcTeleopBotScore = 0;
+pcTeleopTopScore = 0;
+‪pcTeleopInScore = 0;
+‪pcTeleopBotAttempt = 0;
+‪pcTeleopTopAttempt = 0;
+‪pcTeleopInAttempt = 0;
+
 ‪revolutionsCheck = false;
 ‪colorCheck = false;
 ‪climb = false;
 ‪park = false;
 
-var counters = [document.getElementById("counter1"), document.getElementById("counter2"), document.getElementById("counter3"), document.getElementById("counter4"), document.getElementById("counter5")]
+var counters = [document.getElementById("counter1"), document.getElementById("counter2"), document.getElementById("counter3"), document.getElementById("counter4"), document.getElementById("counter5")];
+var autonVars = [pcAutonBotScore, pcAutonTopScore, pcAutonInScore, pcAutonBotAttempt, pcAutonTopAttempt, pcAutonInAttempt];
+var teleopVars = [pcTeleopBotScore, pcTeleopTopScore, pcTeleopInScore, pcTeleopBotAttempt, pcTeleopTopAttempt, pcTeleopInAttempt];
 
 
 //Endgame (THIS YEAR)
